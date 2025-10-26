9e8cf6ef4b962d990aa5a4e896a5f999e4f5bc31
Bhavya Saini <bhavyasaini@Bhavyas-MacBook-Pro.local>
2025-10-26 09:20:18 -0400

    style changes

--- w05/src/main/java/ca/utoronto/utm/SuperGradeTracker/SyllabusView.java
+++ w05/src/main/java/ca/utoronto/utm/SuperGradeTracker/SyllabusView.java
@@ -1,6 +1,7 @@
 package ca.utoronto.utm.SuperGradeTracker;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
@@ -12,8 +13,32 @@ public class SyllabusView extends VBox {
     private Label statusLabel = new Label();
 
     public SyllabusView() {
-        this.setSpacing(10);
+        this.setSpacing(20);
         this.setPadding(new Insets(10));
+        this.setAlignment(Pos.CENTER);
+        this.setStyle("-fx-font-size: 36px;");
+
+
+
+        selectPDFButton.setStyle(
+                "-fx-background-color: #4CAF50; " +  // green
+                        "-fx-text-fill: white; " +
+                        "-fx-font-weight: bold; " +
+                        "-fx-background-radius: 8; " +
+                        "-fx-padding: 8 16; " +
+                        "-fx-cursor: hand;"
+        );
+
+        generateCSVButton.setStyle(
+                "-fx-background-color: #2196F3; " +  // blue
+                        "-fx-text-fill: white; " +
+                        "-fx-font-weight: bold; " +
+                        "-fx-background-radius: 8; " +
+                        "-fx-padding: 8 16; " +
+                        "-fx-cursor: hand;"
+        );
+
+
 
         this.getChildren().addAll(
                 new Label("Upload your syllabus PDF:"),
