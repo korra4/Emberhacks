73eddccf484cd6e0754adf5e6a683140628002d8
Bhavya Saini <bhavyasaini@Bhavyas-MacBook-Pro.local>
2025-10-25 21:42:03 -0400

    OPEN w05 it should work there :)

--- w05/src/main/java/module-info.java
+++ w05/src/main/java/module-info.java
@@ -2,13 +2,5 @@ module w05 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    opens ca.utoronto.utm.spaghettiClock to javafx.fxml;
-    opens ca.utoronto.utm.nonMVCballoon to javafx.fxml;
-    opens ca.utoronto.utm.balloonmvc to javafx.fxml;
-    opens ca.utoronto.utm.numbersmvc to javafx.fxml;
-    exports ca.utoronto.utm.spaghettiClock;
-    exports ca.utoronto.utm.nonMVCballoon;
-    exports ca.utoronto.utm.balloonmvc;
-    exports ca.utoronto.utm.numbersmvc;
     exports ca.utoronto.utm.SuperGradeTracker to javafx.graphics;
 }
